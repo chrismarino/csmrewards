3d20c11a98051e8766b0fad3849cc29b481d7e41
Chris Marino <chris@promptone.ai>
2024-12-09 10:22:58 -0800

    Use correct signature. Refactor and enhance share calculation functions; remove unused Etherscan data retrieval

--- csmrewards.js
+++ csmrewards.js
@@ -1,15 +1,12 @@
 // csmrewards.js
 const axios = require("axios");
 const dotenv = require("dotenv");
+dotenv.config();
 const { getCumulativeFeeShares } = require("./lib/getCumulativeFeeShares");
 const { getDistributedShares } = require("./lib/getDistributedShares");
-const { getPooledEthShares } = require("./lib/getPooledEthShares");
+const { getPooledEthByShares } = require("./lib/getPooledEthByShares");
 const { getTotalBond } = require("./lib/getTotalBond");
-const { getClaimableBondShares } = require("./lib/getClaimableBondShares");
-const getEtherscanData = require("./lib/getEtherscanData");
-
-// Load environment variables from .env file
-dotenv.config();
+const { getExcessBondShares } = require("./lib/getExcessBondShares");
 
 // Get the GitHub URL and rewards proof file from environment variables
 const githubUrl = process.env.GITHUB_URL;
@@ -24,30 +21,21 @@ async function csmrewards(nodeOperatorID) {
   }
 // Get the cumulative fees from the github proof file.
   const cumulativeFeeShares = await getCumulativeFeeShares(nodeOperatorID);
-
-  //console.log(`Cumulative Fee Shares:`, cumulativeFeeShares);
-  const totalRewardsEth = await getPooledEthShares(cumulativeFeeShares.cumulativeFeeShares);
-  console.log(`Total Eth Rewards:`, totalRewardsEth);
-
-  // const claimableBondShares = await getClaimableBondShares(nodeOperatorID);
-  // console.log(`Total Claimable Bond Shares:`, claimableBondShares);
-
+  const totalRewardsEth = await getPooledEthByShares(cumulativeFeeShares.cumulativeFeeShares);
+  const excessBondShares = await getExcessBondShares(nodeOperatorID);
+  const totalClaimableRewardsEth = await getPooledEthByShares(excessBondShares.excessBondShares);
+  const totalRequiredBond = await getPooledEthByShares(excessBondShares.requiredBondShares);
   const distributedEth = await getDistributedShares(nodeOperatorID);
-  console.log(`Total Distributed Eth:`, distributedEth ?? 0);
-
-  const totalBondEth = await getTotalBond(nodeOperatorID);
-  console.log(`Total Bond Eth:`, totalBondEth);
-
-
+  console.log(excessBondShares.keys, "Keys:");
+  console.log(totalRequiredBond.toFixed(4), "Total Required Bond:");
+  console.log(totalRewardsEth.toFixed(4), "Total Eth Rewards:");
+  console.log(totalClaimableRewardsEth.toFixed(4), "Excess Bond:");
+  console.log((distributedEth ?? 0).toFixed(4), "Total Distributed Eth:");
+  console.log((totalRequiredBond + totalRewardsEth + totalClaimableRewardsEth).toFixed(4), "Total Node Operator Eth:");
 }
 
 // Export the functions
-module.exports = {
-  csmrewards,
-  getPooledEthShares,
-  getDistributedShares,
-  getTotalBond
-};
+module.exports = { csmrewards};
 
 // If the script is run directly, call the csmrewards function with the command line argument
 if (require.main === module) {
